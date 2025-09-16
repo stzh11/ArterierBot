cf9997f9975e4ec1f8d007469c85b00c2a04e431
stzh11 <stepan.zhukov.64@mail.ru>
2025-09-16 10:03:12 +0300

    Удаление не нужных файлов

--- script.py
+++
@@ -1,6 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("8333896356:AAERB58Gb6k6scltmwf25j053nShy49NySo")
-
-bot.remove_webhook()
-bot.set_webhook("https://functions.yandexcloud.net/d4eilkdcnq5n6inkdvl2")
